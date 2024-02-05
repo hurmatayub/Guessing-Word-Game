05f0d9ca7d7d0aaef5723f3f3cfb085e3c746f33
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-02-05 22:18:58 +0500

    Add files via upload

---
+++ HurmatAyub.py
@@ -0,0 +1,121 @@
+
+print("What is PYTHON?")
+a = "a:High level Programming Language"
+b = "b:Hyper Taxt Markup Language"
+c = "c:Easy and Basic Language"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "a":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"a\"\n\t")
+
+
+print("What is VARIABLES?")
+a = "a:Box"
+b = "b:Adjective"
+c = "c:Container"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "c":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"c\"\n\t")
+   
+
+print("What is the output of the following code\na=\"2\"\nb= \"9\"\nprint(a+b)")
+a = "a:29"
+b = "b:11"
+c = "c:Erorr"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "a":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"a\"\n\t")
+   
+
+
+print("How many types of LOOPS?")
+a = "a:2"
+b = "b:3"
+c = "c:4\n\t"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "b":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"b\"\n\t")
+
+
+print("What is WHILE LOOP?")
+a = "a:With the while loop we can execute a set of statements as long as a condition is true."
+b = "b:With the while loop we can return data as a result"
+c = "c:With the while loop we can execute a set of statements"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "a":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"a\"\n\t")
+   
+
+print("What is FOR LOOP?")
+a = "a:With the for loop we can execute a set of statements as long as a condition is true."
+b = "b:With the for loop we can return data as a result"
+c = "c:With the for loop we can execute a set of statements"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "c":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"c\"\n\t")
+   
+   
+
+print("What is FUNCTIONS?")
+a = "a:An occasion."
+b = "b:A function is a block of code which only runs when it is called."
+c = "c:A functiion is a special variable"
+print(a)
+print(b)
+print(c)
+select=input("Select any one option:")
+if select== "b":
+   print("CORRECT!!!\n\t")
+else:
+   print("YOU LOOSE\n\t")
+   print("CORRECT OPTION IS\"b\"\n\t")
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
